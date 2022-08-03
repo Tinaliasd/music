77c5a327b66384f631a62b2fe4ee9621fe25bb04
Tinaliasd <2469843239@qq.com>
2022-08-03 15:44:29 +0800

    测试文件

---
+++ music-admin/src/main/java/SpringBootStart.java
@@ -0,0 +1,13 @@
+/**
+ * @program: music
+ * @description: 启动类
+ * @author: Tinaliasd
+ **/
+
+public class SpringBootStart {
+
+    public static void main(String[] args) {
+        System.out.println("你好");
+
+    }
+}
